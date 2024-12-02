0966308012ddf11ede1889ad3bdcb80d87e004f6
Jon Cahill <jon@zero41.com>
2024-12-02 19:12:10 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["eslint-config-zero41-react", "plugin:react-native/all"],
+  plugins: ["react-native"],
+  env: {
+    "react-native/react-native": true,
+  },
+  rules: {},
+};
